5fd5601556a829381b96f6711c2bbf40978e0975
Chanathip Kittikaiwan <chanathi@ais.co.th>
2024-06-18 08:54:07 +0700

    feat: add Swagger to service

--- packages/service/src/main.ts
+++ packages/service/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors();
-    await app.listen(3000);
+
+    const config = new DocumentBuilder()
+        .setTitle('Library Management System')
+        .setDescription('A backend API utilized MongoDB to manage the library')
+        .setVersion('1.0')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
+    await app.listen(process.env.PORT);
+    console.log(`Service start on http://localhost:${process.env.PORT}`);
 }
 bootstrap();
