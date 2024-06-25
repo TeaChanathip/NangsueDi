138ea93d5f88a4c63cd2afc634b9b58499b78830
Chanathip Kittikaiwan <chanathi@ais.co.th>
2024-06-26 00:45:26 +0700

    service feat: add books draft api

--- packages/service/src/modules/books/books.controller.ts
+++ packages/service/src/modules/books/books.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Patch,
+    Post,
+    Query,
+} from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { Roles } from 'src/common/decorators/roles.decorator';
 import { Role } from 'src/shared/enums/role.enum';
@@ -6,6 +15,7 @@ import { BooksRegisterReqDto } from './dtos/books.register.req.dto';
 import { BooksService } from './books.service';
 import { BooksSearchReqDto } from './dtos/books.search.req.dto';
 import { PublicRoute } from 'src/common/decorators/public-route.decorator';
+import { Types } from 'mongoose';
 
 @ApiTags('Book')
 @Controller('books')
@@ -17,6 +27,27 @@ export class BooksController {
     @Post('register')
     async register(@Body() booksRegisterReqDto: BooksRegisterReqDto) {
         return await this.booksService.register(booksRegisterReqDto);
+    }
+
+    @ApiBearerAuth()
+    @Roles(Role.MANAGER, Role.ADMIN)
+    @Patch(':bookId')
+    async update(@Param('bookId') bookId: Types.ObjectId) {
+        // logic
+    }
+
+    @ApiBearerAuth()
+    @Roles(Role.MANAGER, Role.ADMIN)
+    @Get(':bookId')
+    async get(@Param('bookId') bookId: Types.ObjectId) {
+        // logic
+    }
+
+    @ApiBearerAuth()
+    @Roles(Role.MANAGER, Role.ADMIN)
+    @Delete(':bookId')
+    async delete(@Param('bookId') bookId: Types.ObjectId) {
+        // logic
     }
 
     @PublicRoute()
