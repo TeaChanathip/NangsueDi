bc81cad69e60ba893bee4beb549c1a66ab437b46
Chanathip Kittikaiwan <chanathi@ais.co.th>
2024-07-08 11:11:38 +0700

    client feat: setup prettier and fonts

---
+++ packages/client/tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [],
+	theme: {
+		extend: {
+			color: {
+				primary: {
+					10: '#F6FBFA',
+					20: '#C9E8E3',
+					50: '#F0F6D6',
+					100: '#B5CE3C',
+					200: '#A0B72E',
+					300: '#59661A',
+					400: '#5EBBA9',
+					500: '#29685D',
+					600: '#25584F',
+					700: '#1B4039',
+				},
+				white: {
+					100: '#F5F5F5' ,
+				},
+				red: '#FF6961',
+			},
+		},
+	},
+	plugins: [],
+};
