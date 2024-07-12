b5a2742c105d9ee5e904a19a9df52dd920de9e5b
Chanathip Kittikaiwan <chanathi@ais.co.th>
2024-07-12 13:07:41 +0700

    client feat: update register step 2 template

--- packages/client/src/app/routes/register/components/second-step/second-step.component.ts
+++ packages/client/src/app/routes/register/components/second-step/second-step.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
+import { ReactiveFormsModule, ValidationErrors } from '@angular/forms';
+import { FormInputComponent } from '../../../../shared/components/form-input/form-input.component';
+import { WarningMsgComponent } from '../../../../shared/components/warning-msg/warning-msg.component';
+import { getWarning } from '../../../../shared/utils/getWarning';
 
 @Component({
 	selector: 'app-second-step',
 	standalone: true,
-	imports: [],
+	imports: [ReactiveFormsModule, FormInputComponent, WarningMsgComponent],
 	templateUrl: './second-step.component.html',
 	styleUrl: './second-step.component.scss',
 })
-export class SecondStepComponent {}
+export class SecondStepComponent implements OnChanges {
+	@Input() phoneNumberErrors: ValidationErrors | null = null;
+	@Input() birthDateErrors: ValidationErrors | null = null;
+
+	phoneNumberErrMsg: string = 'empty';
+	birthDateErrMsg: string = 'empty';
+
+	ngOnChanges(): void {
+		if (this.phoneNumberErrors) {
+			console.log(this.phoneNumberErrors);
+			// this.phoneNumberErrMsg = getWarning(this.phoneNumberErrors);
+		}
+		if (this.birthDateErrors) {
+			console.log(this.birthDateErrors);
+			// this.birthDateErrMsg = getWarning(this.birthDateErrors);
+		}
+	}
+}
